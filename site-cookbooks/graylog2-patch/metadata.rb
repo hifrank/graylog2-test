02549f107bcbf400594b18ec3075cf88a1a0cec1
hifrank <r7692118@gmail.com>
2012-08-21 10:41:34 +0800

    add dependcy

--- site-cookbooks/graylog2-patch/metadata.rb
+++ site-cookbooks/graylog2-patch/metadata.rb
@@ -1,6 +1,9 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "hifrank"
+maintainer_email "r7692118@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures graylog2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends "rvm"
+depends "graylog2"
